6a45fd96151f46f601d6bee6618843c0a606b7e1
lemmmon <1252898650@qq.com>
2017-12-24 17:17:35 +0800

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,22 @@
+package toolsMadeByMyself;
+
+public class Node {
+	int key;	// class
+	double val;	//value
+	public Node(int key, double val) {
+		this.key = key;
+		this.val = val;
+	}
+	public int getKey() {
+		return key;
+	}
+	public void setKey(int key) {
+		this.key = key;
+	}
+	public double getVal() {
+		return val;
+	}
+	public void setVal(double val) {
+		this.val = val;
+	}
+}
